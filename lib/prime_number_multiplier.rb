07067274017a25b043efab92ad86f05d0e0340f8
aimee daniells <aimeedaniells@googlemail.com>
2012-08-23 23:14:31 +0100

    First valid implementation i can come up with.

--- lib/prime_number_multiplier.rb
+++ lib/prime_number_multiplier.rb
@@ -1,7 +1,51 @@
+require 'prime'
+
 class PrimeNumberMultiplier
 
+  attr_reader :top_row, :left_column, :output
+
+  def initialize
+    @top_row = Prime.take 10
+    @left_column = Prime.take 10
+    @output = @left_column.reduce([]) {|output, multiplier|
+      output << @top_row.map {|value| multiplier * value }
+    }
+  end
+
+  def left_column_width
+    @left_column.max.to_s.length
+  end
+
+  def column_widths
+    @output.transpose.map {|column| column.max.to_s.length }
+  end
+
+  def print_contents! contents
+    string = ''
+    contents.each_with_index {|value, index|
+      string += value.to_s.rjust(column_widths[index] + 1)
+    }
+    string
+  end
+
+  def print_top_row!
+    ''.rjust(left_column_width) + " |" + print_contents!(@top_row)
+  end
+
+  def print_divider_row!
+    print_top_row!.gsub(/./, '-')
+  end
+
+  def print_row!(multiplier, contents)
+    multiplier.to_s.rjust(left_column_width) + " |" + print_contents!(contents)
+  end
+
   def print_output!
-    "     |  2   3   5   7  11  13  17  19  23  29\n  -------------------------------------------\n   2 |  4   6  10  14  22  26  34  38  46  58\n   3 |  6   9  15  21  33  39  51  57  69  87\n   5 | 10  15  25  35  55  65  85  95 115 145\n   7 | 14  21  35  49  77  91 119 133 161 203\n  11 | 22  33  55  77 121 143 187 209 253 319\n  13 | 26  39  65  91 143 169 221 247 299 377\n  17 | 34  51  85 119 187 221 289 323 391 493\n  19 | 38  57  95 133 209 247 323 361 437 551\n  23 | 46  69 115 161 253 299 391 437 529 667\n  29 | 58  87 145 203 319 377 493 551 667 841"
+    lines = [print_top_row!, print_divider_row!]
+    @left_column.each_with_index {|multiplier, row|
+      lines << print_row!(multiplier, @output[row])
+    }
+    lines.join("\n")
   end
 
 end
---
+++ spec/prime_number_multiplier_spec.rb
@@ -0,0 +1,60 @@
+require 'prime_number_multiplier'
+
+describe 'PrimeNumberMultiplier' do
+
+  let(:multiplier) { PrimeNumberMultiplier.new }
+
+  it 'has prime numbers across the top' do
+    multiplier.top_row.should == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+  end
+
+  it 'has prime numbers in the left column' do
+    multiplier.left_column.should == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+  end
+
+  it 'already knows the output' do
+    multiplier.output.should == [
+      [4, 6, 10, 14, 22, 26, 34, 38, 46, 58],
+      [6, 9, 15, 21, 33, 39, 51, 57, 69, 87],
+      [10, 15, 25, 35, 55, 65, 85, 95, 115, 145],
+      [14, 21, 35, 49, 77, 91, 119, 133, 161, 203],
+      [22, 33, 55, 77, 121, 143, 187, 209, 253, 319],
+      [26, 39, 65, 91, 143, 169, 221, 247, 299, 377],
+      [34, 51, 85, 119, 187, 221, 289, 323, 391, 493],
+      [38, 57, 95, 133, 209, 247, 323, 361, 437, 551],
+      [46, 69, 115, 161, 253, 299, 391, 437, 529, 667],
+      [58, 87, 145, 203, 319, 377, 493, 551, 667, 841]
+    ]
+  end
+
+  it 'calculates the column widths' do
+    multiplier.column_widths.should == [2, 2, 3, 3, 3, 3, 3, 3, 3, 3]
+  end
+
+  it 'calculates the left column width' do
+    multiplier.left_column_width.should == 2
+  end
+
+  describe 'printing' do
+
+    it 'can print the top row' do
+      multiplier.print_top_row!.should == "   |  2  3   5   7  11  13  17  19  23  29"
+    end
+
+    it 'can print a divider row of the correct length' do
+      multiplier.print_divider_row!.should == "------------------------------------------"
+    end
+
+    it 'can print a row given a multiplier and contents' do
+      multiplier.print_row!(2, [4, 6, 10, 14, 22, 26, 34, 38, 46, 58])
+        .should == " 2 |  4  6  10  14  22  26  34  38  46  58"
+    end
+
+    it 'can correctly justify the number on the left' do
+      multiplier.print_row!(29, [58, 87, 145, 203, 319, 377, 493, 551, 667, 841])
+        .should == "29 | 58 87 145 203 319 377 493 551 667 841"
+    end
+
+  end
+
+end
